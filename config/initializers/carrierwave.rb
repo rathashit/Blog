ff023876db950c463f55abd4919213cce1fe6349
rathashit <rathashit@gmail.com>
2015-10-18 15:12:21 +0530

    change

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-	config.fog_credentials = {
-		provider: "AWS",
-		aws_access_key_id: 'AKIAJ3NALMB7IO5KUH6A',
-		aws_secret_access_key: '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',
-		region: 'ap-southeast-1aaaalkdsanfkl'
-	}
-	config.fog_directory = 'humblepostimages'
-end
